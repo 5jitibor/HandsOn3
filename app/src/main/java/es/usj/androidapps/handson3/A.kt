0cf62898de03f028ec671084bb1179fbc1af78fa
Jose Ignacio Trasobares <5jitibor@gmail.com>
2021-03-04 19:53:38 +0100

    Initial commit

---
+++ app/src/main/java/es/usj/androidapps/handson3/A.kt
@@ -0,0 +1,11 @@
+package es.usj.androidapps.handson3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class A : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a)
+    }
+}
